2a5d73de6e0dabecff00a0b3f97aa1ff6df5941c
josh <jrenner@nextp12.ific.uv.es>
2016-02-15 17:18:35 +0100

    Creates 2D projection from 3D data; training step is now a separate run; signal and background test results reported

--- dnn2d/dnn2d_conv.py
+++ dnn2d/dnn2d_conv.py
@@ -13,108 +13,101 @@ import numpy as np
 import tensorflow as tf
 
 fdir = "/Users/jrenner/IFIC/dnn/tracks/data"
-rname = "dnn2d_4mm"
+rname = "dnn3d_9mm_28x28x28"
 
-fname_train_si = "{0}/vox_{1}_train_si.h5".format(fdir,rname)
-fname_train_bg = "{0}/vox_{1}_train_bg.h5".format(fdir,rname)
-
-fname_test_si = "{0}/vox_{1}_test_si.h5".format(fdir,rname)
-fname_test_bg = "{0}/vox_{1}_test_bg.h5".format(fdir,rname)
+training_run = False;                           # run training step
+test_eval_only = True and (not training_run);  # only read test data (cannot be True while training)
     
 # Input variables
-vox_ext = 96
-vox_size = 4
+vox_ext = 126
+vox_size = 9
 nclass = 2
 
-num_batches = 2000
-batch_size = 100
+num_batches = 6000 #2000
+batch_size = 100 #100
+
+ntrain_evts = 90000     # number of training evts per dataset (remainder used for test)
 
 # Calculated parameters
 pdim = int(2 * vox_ext / vox_size)
 npix = pdim * pdim
 print "Found dim of {0} for {1} pixels.".format(pdim,npix)
 
+# Constructed file names.
+fname_si = "{0}/vox_{1}_si.h5".format(fdir,rname)
+fname_bg = "{0}/vox_{1}_bg.h5".format(fdir,rname)
+fn_saver = "tfmdl_{0}_pix_{1}_train_{2}.ckpt".format(rname,npix,ntrain_evts)   # for saving trained network
+
 # -----------------------------------------------------------------------------
 # Read in all the data.
 # -----------------------------------------------------------------------------
-dat_train_si = []; lbl_train_si = []
-dat_train_bg = []; lbl_train_bg = []
+dat_si = []; lbl_si = []
+dat_bg = []; lbl_bg = []
 
-dat_test_si = []; lbl_test_si = []
-dat_test_bg = []; lbl_test_bg = []
-
-# Training, signal
-h5f_train_si = h5py.File(fname_train_si,'r')
-ntrk = 0
-while(ntrk < len(h5f_train_si)):
-  trkn = h5f_train_si['trk{0}'.format(ntrk)]
+# Signal
+h5f_si = h5py.File(fname_si,'r')
+if(test_eval_only):                                  # only read test data 
+    ntrk = ntrain_evts; nsi_evts = len(h5f_si)
+elif(training_run):                                  # only read training data
+    ntrk = 0; nsi_evts = ntrain_evts
+else:                                                # read all data
+    ntrk = 0; nsi_evts = len(h5f_si)
+    
+print "Reading signal events...";
+while(ntrk < nsi_evts):
+  trkn = h5f_si['trk{0}'.format(ntrk)]
   darr = np.zeros(npix)
   xarr = trkn[0]; yarr = trkn[1]; earr = trkn[2]
   for xx,yy,ee in zip(xarr,yarr,earr):
-      darr[int(yy*pdim + xx)] = ee
+      darr[int(yy*pdim + xx)] += ee
   darr *= 1./max(darr)
-  dat_train_si.append(darr)
-  lbl_train_si.append([1, 0])
+  dat_si.append(darr)
+  lbl_si.append([1, 0])
   ntrk += 1
-print "Read {0} signal training events".format(len(dat_train_si))
-h5f_train_si.close()
+  
+  if(ntrk % int(nsi_evts/100) == 0): print "Read {0}% ...".format(int(100.0*ntrk/nsi_evts));
+print "Read {0} signal events".format(len(dat_si))
+h5f_si.close()
 
 # Training, background
-h5f_train_bg = h5py.File(fname_train_bg,'r')
-ntrk = 0
-while(ntrk < len(h5f_train_bg)):
-  trkn = h5f_train_bg['trk{0}'.format(ntrk)]
+h5f_bg = h5py.File(fname_bg,'r')
+if(test_eval_only):                                  # only read test data 
+    ntrk = ntrain_evts; nbg_evts = len(h5f_bg)
+elif(training_run):                                  # only read training data
+    ntrk = 0; nbg_evts = ntrain_evts
+else:                                                # read all data
+    ntrk = 0; nbg_evts = len(h5f_bg)
+    
+while(ntrk < nbg_evts):
+  trkn = h5f_bg['trk{0}'.format(ntrk)]
   darr = np.zeros(npix)
   xarr = trkn[0]; yarr = trkn[1]; earr = trkn[2]
   for xx,yy,ee in zip(xarr,yarr,earr):
-      darr[int(yy*pdim + xx)] = ee
+      darr[int(yy*pdim + xx)] += ee
   darr *= 1./max(darr)
-  dat_train_bg.append(darr)
-  lbl_train_bg.append([0, 1])
+  dat_bg.append(darr)
+  lbl_bg.append([0, 1])
   ntrk += 1
-print "Read {0} background training events".format(len(dat_train_bg))
-h5f_train_bg.close()
+  
+  if(ntrk % int(nbg_evts/100) == 0): print "Read {0}% ...".format(int(100.0*ntrk/nbg_evts));
+print "Read {0} background events".format(len(dat_bg))
+h5f_bg.close()
 
-# Test, signal
-h5f_test_si = h5py.File(fname_test_si,'r')
-ntrk = 0
-while(ntrk < len(h5f_test_si)):
-  trkn = h5f_test_si['trk{0}'.format(ntrk)]
-  darr = np.zeros(npix)
-  xarr = trkn[0]; yarr = trkn[1]; earr = trkn[2]
-  for xx,yy,ee in zip(xarr,yarr,earr):
-      darr[int(yy*pdim + xx)] = ee
-  darr *= 1./max(darr)
-  dat_test_si.append(darr)
-  lbl_test_si.append([1, 0])
-  ntrk += 1
-print "Read {0} signal test events".format(len(dat_test_si))
-h5f_test_si.close()
-
-# Test, background
-h5f_test_bg = h5py.File(fname_test_bg,'r')
-ntrk = 0
-while(ntrk < len(h5f_test_bg)):
-  trkn = h5f_test_bg['trk{0}'.format(ntrk)]
-  darr = np.zeros(npix)
-  xarr = trkn[0]; yarr = trkn[1]; earr = trkn[2]
-  for xx,yy,ee in zip(xarr,yarr,earr):
-      darr[int(yy*pdim + xx)] = ee
-  darr *= 1./max(darr)
-  dat_test_bg.append(darr)
-  lbl_test_bg.append([0, 1])
-  ntrk += 1
-print "Read {0} background test events".format(len(dat_test_bg))
-h5f_test_bg.close()
-
-
-# Create one large training dataset.
-dat_train = np.concatenate((dat_train_si, dat_train_bg))
-lbl_train = np.concatenate((lbl_train_si, lbl_train_bg))
-
-# Create one large test dataset.
-dat_test = np.concatenate((dat_test_si, dat_test_bg))
-lbl_test = np.concatenate((lbl_test_si, lbl_test_bg))
+# Create one large dataset for test and training datasets.
+if(test_eval_only):
+    dat_train = []; dat_train_si = []; dat_train_bg = []
+    lbl_train = []; lbl_train_si = []; lbl_train_bg = []
+    
+    dat_test_si = dat_si; dat_test_bg = dat_bg
+    lbl_test_si = lbl_si; lbl_test_bg = lbl_bg
+else:
+    dat_train_si = dat_si[0:ntrain_evts]; dat_train_bg = dat_bg[0:ntrain_evts]
+    lbl_train_si = lbl_si[0:ntrain_evts]; lbl_train_bg = lbl_bg[0:ntrain_evts]
+    dat_train = np.concatenate((dat_train_si, dat_train_bg))
+    lbl_train = np.concatenate((lbl_train_si, lbl_train_bg))
+    
+    dat_test_si = dat_si[ntrain_evts:]; dat_test_bg = dat_bg[ntrain_evts:]
+    lbl_test_si = lbl_si[ntrain_evts:]; lbl_test_bg = lbl_bg[ntrain_evts:]
 
 # -----------------------------------------------------------------------------
 # Define helper methods.
@@ -157,9 +150,10 @@ h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
 # Densely connected layer
-W_fc1 = weight_variable([12 * 12 * 64, 1024])
+new_dim = int(pdim / 4)
+W_fc1 = weight_variable([new_dim * new_dim * 64, 1024])
 b_fc1 = bias_variable([1024])
-h_pool2_flat = tf.reshape(h_pool2, [-1, 12*12*64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, new_dim*new_dim*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 # Dropout
@@ -177,35 +171,56 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 sess = tf.Session()
-sess.run(tf.initialize_all_variables())
+init_op = tf.initialize_all_variables()
+sess.run(init_op)
 
-# Train the NN in batches.
-nbatch_dat = len(dat_train) / batch_size  # number of batches that fit in the data length
-for bnum in range(num_batches):
-    
-    print "Training batch {0} of {1}".format(bnum,num_batches)
-    
-    # Shuffle the data if we reached the end of the loop.
-    #if(bnum == 0 or (bnum*batch_size < len(dat_train) and (bnum+1)*batch_size >= len(dat_train))):
-    if(bnum % nbatch_dat == 0):
-    
-      print "Shuffling data..."
-      perm = np.arange(len(dat_train))
-      np.random.shuffle(perm)
-      dat_train = dat_train[perm]
-      lbl_train = lbl_train[perm]
+# Create a saver to save the DNN.
+saver = tf.train.Saver()
 
-    # Be sure not to go over the number of batches that fit in the data length.
-    btemp = bnum % nbatch_dat
+# Restore saved variables if the option is set.
+if(not training_run):
+    print "Restoring variables read from file {0} ...".format(fn_saver)
+    saver.restore(sess,fn_saver)
+else:
+
+    # Train the NN in batches.
+    nbatch_dat = len(dat_train) / batch_size  # number of batches that fit in the data length
+    for bnum in range(num_batches):
+        
+        print "Training batch {0} of {1}".format(bnum,num_batches)
+        
+        # Shuffle the data if we reached the end of the loop.
+        #if(bnum == 0 or (bnum*batch_size < len(dat_train) and (bnum+1)*batch_size >= len(dat_train))):
+        if(bnum % nbatch_dat == 0):
+        
+          print "Shuffling data..."
+          perm = np.arange(len(dat_train))
+          np.random.shuffle(perm)
+          dat_train = dat_train[perm]
+          lbl_train = lbl_train[perm]
     
-    batch_xs = dat_train[btemp*batch_size:(btemp + 1)*batch_size,:]
-    batch_ys = lbl_train[btemp*batch_size:(btemp + 1)*batch_size,:]
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+        # Be sure not to go over the number of batches that fit in the data length.
+        btemp = bnum % nbatch_dat
+        
+        batch_xs = dat_train[btemp*batch_size:(btemp + 1)*batch_size,:]
+        batch_ys = lbl_train[btemp*batch_size:(btemp + 1)*batch_size,:]
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+    
+    # Save the trained model.
+    print "Saving trained model to: {0}".format(fn_saver)
+    save_path = saver.save(sess, fn_saver)
 
 # Evaluate the performance.
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-print "On test data:"
-print sess.run(accuracy, feed_dict={x: dat_test, y_: lbl_test, keep_prob: 1.0})
-print "On training data:"
-print sess.run(accuracy, feed_dict={x: dat_train, y_: lbl_train, keep_prob: 1.0})
+if(not training_run):
+    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print "On test signal data:"
+    print sess.run(accuracy, feed_dict={x: dat_test_si[0:1000], y_: lbl_test_si[0:1000], keep_prob: 1.0})
+    print "On test background data:"
+    print sess.run(accuracy, feed_dict={x: dat_test_bg[0:1000], y_: lbl_test_bg[0:1000], keep_prob: 1.0})
+
+    if(not test_eval_only):
+        print "On training signal data:"
+        print sess.run(accuracy, feed_dict={x: dat_train_si[0:1000], y_: lbl_train_si[0:1000], keep_prob: 1.0})
+        print "On training background data:"
+        print sess.run(accuracy, feed_dict={x: dat_train_bg[0:1000], y_: lbl_train_bg[0:1000], keep_prob: 1.0})
